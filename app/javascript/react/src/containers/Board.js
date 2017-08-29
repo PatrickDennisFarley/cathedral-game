208afd784fd20abbeafbd95c319358ee673c9923
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-29 19:22:49 -0400

    got flexbox board displaying to the screen

--- app/javascript/react/src/containers/Board.js
+++ app/javascript/react/src/containers/Board.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import BoardTile from '../components/BoardTile';
 
 class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      board: this.initializeBoard()
     }
+    this.initializeBoard = this.initializeBoard.bind(this)
+  }
+
+  initializeBoard(){
+    let newBoard = [];
+    for (let i = 0; i < 10; i++) {
+      newBoard.push(new Array(10).fill(0));
+    }
+    return newBoard;
   }
 
   render() {
+    let flattenedBoard = [].concat.apply([], this.state.board);
+    console.log(flattenedBoard)
+    let boardDisplay = flattenedBoard.map((elementData, index) => {
+      return (
+        <BoardTile
+          key={index}
+          value={elementData}
+        />
+      )
+    })
     return (
-      <h1>Hello from inside Board.js</h1>
+      <div className="board">
+        {boardDisplay}
+      </div>
     )
   }
 }
