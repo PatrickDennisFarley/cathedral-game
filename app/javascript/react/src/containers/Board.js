03549c0ad5a77b3330d551336f2fe2b33c4d1553
Patrick Farley <patrick.dennis.farley@gmail.com>
2017-08-29 18:19:08 -0400

    added board container & configured react-router

---
+++ app/javascript/react/src/containers/Board.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>Hello from inside Board.js</h1>
+    )
+  }
+}
+
+
+export default Board
